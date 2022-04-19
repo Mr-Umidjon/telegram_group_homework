e924b4ce622755374b11358d51f567e0b74f1424
Umidjon <mrumidjon2020@gmail.com>
2022-04-19 13:23:03 +0500

    This function will find all the users in the json file and return the list of users id 01

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -1,6 +1,7 @@
 from read_data import read_data
 
-def find_all_users_id(data: dict)->list:
+
+def find_all_users_id(data: dict) -> list:
     """ 
     This function will find all the users in the json file and return the list of users id
     
@@ -9,4 +10,14 @@ def find_all_users_id(data: dict)->list:
     Returns:
         list: List containing all the users id
     """
-    return
+    users_ids = []
+    for message in data['messages']:
+        users_id = message.get("actor_id")
+        if users_id and users_id not in users_ids:
+            users_ids.append(users_id)
+    return users_ids
+
+
+data = read_data("data/result.json")
+print(find_all_users_id(data))
+# print(data['messages'][0]['actor_id'])
